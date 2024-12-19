c8783789cf24e429929dd2544402ef279b4cb897
Honglei Huang <Honglei1.Huang@amd.com>
2024-12-19 15:01:20 +0800

    hsakmt: format event command code
    Change-Id: I3e131006d1be1c5af9f6e7975ece3e792b371bc1
    Signed-off-by: Honglei Huang <Honglei1.Huang@amd.com>

--- src/hsakmt/hsakmt_device.c
+++ src/hsakmt/hsakmt_device.c
@@ -793,15 +793,14 @@ vhsakmt_ccmd_event(struct vhsakmt_base_context *bctx, struct vhsakmt_ccmd_req *h
    switch (req->type) {
    case VHSAKMT_CCMD_EVENT_CREATE: {
       VHSA_RSP_ALLOC(ctx, hdr, sizeof(*rsp));
-      HsaEvent *e = NULL;
       struct vhsakmt_object *obj;
+      HsaEvent *e = NULL;
 
-      rsp->ret =
-          hsaKmtCreateEvent((HsaEventDescriptor *)&req->create_args.EventDesc,
-                            req->create_args.ManualReset,
-                            req->create_args.IsSignaled, (HsaEvent **)&e);
+      rsp->ret = hsaKmtCreateEvent(&req->create_args.EventDesc,
+                                   req->create_args.ManualReset,
+                                   req->create_args.IsSignaled, &e);
       if (rsp->ret) {
-         vhsa_err("[EVENT] hsaKmtCreateEvent, hostaddr: %p, id: %d, ret: %d",
+         vhsa_err("Create event failed, address: %p, id: %d, ret: %d",
                   (void *)e, e->EventId, rsp->ret);
          break;
       }
@@ -809,9 +808,10 @@ vhsakmt_ccmd_event(struct vhsakmt_base_context *bctx, struct vhsakmt_ccmd_req *h
       rsp->vevent.event_handle = (uint64_t)e;
 
       obj = vhsakmt_object_create(e, 0, sizeof(*e), VHSAKMT_OBJ_EVENT);
-      assert(obj);
-      vhsakmt_context_object_set_blob_id(ctx, obj, req->blob_id);
+      if (!obj)
+         return -ENOMEM;
 
+      vhsakmt_context_object_set_blob_id(ctx, obj, req->blob_id);
       break;
    }
    case VHSAKMT_CCMD_EVENT_DESTROY: {
@@ -821,46 +821,38 @@ vhsakmt_ccmd_event(struct vhsakmt_base_context *bctx, struct vhsakmt_ccmd_req *h
           vhsakmt_context_get_object_from_res_id(ctx, req->res_id);
 
       if (!obj) {
-         rsp->ret = hsaKmtDestroyEvent(req->event_hanele);
-         break;
-      }
+         if (req->event_hanele)
+            hsaKmtDestroyEvent(req->event_hanele);
+         else {
+            vhsa_err("Invalid event resid: %d and handle: %lx", req->res_id, (uint64_t)req->event_hanele);
+            rsp->ret = -EINVAL;
+            break;
+         }
+      } else
+         vhsakmt_free_object(&ctx->base, &obj->base);
 
-      vhsakmt_free_object(&ctx->base, &obj->base);
-
+      rsp->ret = 0;
       break;
    }
    case VHSAKMT_CCMD_EVENT_SET: {
       VHSA_RSP_ALLOC(ctx, hdr, sizeof(*rsp));
 
       rsp->ret = hsaKmtSetEvent(req->event_hanele);
-      if (rsp->ret)
-         vhsa_err("[EVENT] hsaKmtSetEvent hostaddr: %p, ret: %d",
-                  (void *)req->event_hanele, rsp->ret);
-
       break;
    }
    case VHSAKMT_CCMD_EVENT_RESET: {
       VHSA_RSP_ALLOC(ctx, hdr, sizeof(*rsp));
 
       rsp->ret = hsaKmtResetEvent(req->event_hanele);
-      if (rsp->ret)
-         vhsa_err("[EVENT] hsaKmtResetEvent hostaddr: %p, ret: %d",
-                  (void *)req->event_hanele, rsp->ret);
-
       break;
    }
    case VHSAKMT_CCMD_EVENT_QUERY_STATE: {
       VHSA_RSP_ALLOC(ctx, hdr, sizeof(*rsp));
 
       rsp->ret = hsaKmtQueryEventState(req->event_hanele);
-      if (rsp->ret)
-         vhsa_err("[EVENT] hsaKmtQueryEventState hostaddr: %p, ret: %d",
-                  (void *)req->event_hanele, rsp->ret);
-
       break;
    }
    case VHSAKMT_CCMD_EVENT_WAIT_ON_MULTI_EVENTS: {
-      // TODO
 
       break;
    }
@@ -871,19 +863,21 @@ vhsakmt_ccmd_event(struct vhsakmt_base_context *bctx, struct vhsakmt_ccmd_req *h
       VHSA_CHECK_VA(req->set_trap_handler_args.TrapHandlerBaseAddress);
       VHSA_CHECK_VA(req->set_trap_handler_args.TrapBufferBaseAddress);
 
-      rsp->ret = hsaKmtSetTrapHandler(
-          req->set_trap_handler_args.NodeId,
-          (void *)req->set_trap_handler_args.TrapHandlerBaseAddress,
-          req->set_trap_handler_args.TrapHandlerSizeInBytes,
-          (void *)req->set_trap_handler_args.TrapBufferBaseAddress,
-          req->set_trap_handler_args.TrapHandlerSizeInBytes);
+      rsp->ret =
+          hsaKmtSetTrapHandler(req->set_trap_handler_args.NodeId,
+                               (void *)req->set_trap_handler_args.TrapHandlerBaseAddress,
+                               req->set_trap_handler_args.TrapHandlerSizeInBytes,
+                               (void *)req->set_trap_handler_args.TrapBufferBaseAddress,
+                               req->set_trap_handler_args.TrapHandlerSizeInBytes);
       break;
    }
 
    default:
-      assert(false);
+      vhsa_err("Unsopported event command: %d", req->type);
    }
 
+   if (rsp->ret)
+      vhsa_err("Type: %d ret: %d", req->type, rsp->ret);
    return 0;
 }
 
