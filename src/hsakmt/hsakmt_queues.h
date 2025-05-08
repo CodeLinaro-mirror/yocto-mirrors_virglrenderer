f778e69d47cbed8bd0d2b370ebe1055539690909
Honglei Huang <Honglei1.Huang@amd.com>
2025-05-08 15:13:28 +0800

    hsakmt: move queues cmd into new file
    Split queue cmds into a new file.
    
    Change-Id: I2cac173a920c570065df05e5026508a55d4e600c
    Signed-off-by: Honglei Huang <Honglei1.Huang@amd.com>

---
+++ src/hsakmt/hsakmt_queues.h
@@ -0,0 +1,8 @@
+#ifndef HSAKMT_QUEUES_H
+#define HSAKMT_QUEUES_H
+
+#include "hsakmt_context.h"
+
+int vhsakmt_ccmd_queue(struct vhsakmt_base_context *bctx, struct vhsakmt_ccmd_req *hdr);
+
+#endif /* HSAKMT_QUEUES_H */
